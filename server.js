ad766d00f4fa9c2e96d65a95c3d0d04caba1c92c
Rosie <rosie_ledbrook@msn.com>
2023-01-12 20:04:59 +0000

    Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -22,13 +22,6 @@ app.use(
 );
 
 let server = http.createServer(app);
-server.use(
-  cors({
-    origin: "https://cyoa-online.herokuapp.com",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-)
 
 //Location of views
 app.set('views', path.join(__dirname, '/views'));
